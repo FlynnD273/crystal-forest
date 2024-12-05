c2eda7cdcdd4e9622e4767d22cf70713ab418f4c
Flynn Duniho <flynn@harveyosity.org>
2024-12-05 00:29:37 -0500

    Update rendered animation

--- Assets/Models/Bunny/Renders/_convert.bat
+++ Assets/Models/Bunny/Renders/_convert.bat
@@ -1,1 +1,1 @@
-ffmpeg -start_number 1 -framerate 24 -i %%04d.png -vf yadif -codec:v libx264 -crf 1 -bf 2 -flags +cgop -pix_fmt yuv420p -codec:a aac -strict -2 -b:a 384k -r:a 48000 -movflags faststart _out.mp4 -y
+ffmpeg -start_number 1 -framerate 60 -i %%04d.png -vf yadif -codec:v libx264 -crf 1 -bf 2 -flags +cgop -pix_fmt yuv420p -codec:a aac -strict -2 -b:a 384k -r:a 48000 -movflags faststart _out.mp4 -y
